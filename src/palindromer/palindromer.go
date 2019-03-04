8036746e1e55602a1b5b8a50e3a0f18d11bf2fa4
sampsonsun(孙健俞) <sampsonsun@tencent.com>
2019-03-04 19:46:26 +0800

    update palindrome

--- src/palindromer/palindromer.go
+++ src/palindromer/palindromer.go
@@ -1,15 +1,40 @@
 package palindromer
 
 import (
-
+	"unicode"
 )
+
+//func IsPalindrome(s string) bool {
+//	n := len(s)
+//	
+//	for i, j := 0, n - 1; i < j; i, j = i + 1, j - 1 {
+//		if s[i] != s[j] {
+//			return false
+//		}
+//	}
+//	
+//	return true
+//}
 
 func IsPalindrome(s string) bool {
 	n := len(s)
 	
-	for i, j := 0, n - 1; i < j; i, j = i + 1, j - 1 {
-		if s[i] != s[j] {
+	i, j := 0, n - 1
+	
+	for i < j {
+		for i < j && !unicode.IsDigit(rune(s[i])) && !unicode.IsLetter(rune(s[i])) {
+			i++
+		}
+		
+		for i < j && !unicode.IsDigit(rune(s[j])) && !unicode.IsLetter(rune(s[j])) {
+			j--
+		}
+		
+		if i < j && s[i] != s[j] {
 			return false
+		} else {
+			i++
+			j--
 		}
 	}
 	
