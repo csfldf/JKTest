83201bbe611a42047494a20a96dc29a254a6774a
sampsonsun(孙健俞) <sampsonsun@tencent.com>
2019-03-04 20:28:41 +0800

    resume normal

--- src/palindromer/palindromer.go
+++ src/palindromer/palindromer.go
@@ -1,42 +1,42 @@
 package palindromer
 
 import (
-//	"unicode"
+	"unicode"
 )
-
-func IsPalindrome(s string) bool {
-	n := len(s)
-	
-	for i, j := 0, n - 1; i < j; i, j = i + 1, j - 1 {
-		if s[i] != s[j] {
-			return false
-		}
-	}
-	
-	return true
-}
 
 //func IsPalindrome(s string) bool {
 //	n := len(s)
 //	
-//	i, j := 0, n - 1
-//	
-//	for i < j {
-//		for i < j && !unicode.IsDigit(rune(s[i])) && !unicode.IsLetter(rune(s[i])) {
-//			i++
-//		}
-//		
-//		for i < j && !unicode.IsDigit(rune(s[j])) && !unicode.IsLetter(rune(s[j])) {
-//			j--
-//		}
-//		
-//		if i < j && s[i] != s[j] {
+//	for i, j := 0, n - 1; i < j; i, j = i + 1, j - 1 {
+//		if s[i] != s[j] {
 //			return false
-//		} else {
-//			i++
-//			j--
 //		}
 //	}
 //	
 //	return true
 //}
+
+func IsPalindrome(s string) bool {
+	n := len(s)
+	
+	i, j := 0, n - 1
+	
+	for i < j {
+		for i < j && !unicode.IsDigit(rune(s[i])) && !unicode.IsLetter(rune(s[i])) {
+			i++
+		}
+		
+		for i < j && !unicode.IsDigit(rune(s[j])) && !unicode.IsLetter(rune(s[j])) {
+			j--
+		}
+		
+		if i < j && s[i] != s[j] {
+			return false
+		} else {
+			i++
+			j--
+		}
+	}
+	
+	return true
+}
